cfa93805008c468f32586e7be527616c76b882c4
tee2490 <44152787+tee2490@users.noreply.github.com>
2026-07-02 13:42:35 +0700

    Page8  ทางเลือกเดียว

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,24 +1,39 @@
 ﻿//declare
 string Id;
-double Year, Salary, Bonus=0;
-
+double Salary, PSale, Rate, Num, Bonus=0, Bonus1=0;
 
 //input
 Console.Write("Enter employee ID: ");
-Id= Console.ReadLine();
-
-Console.Write("Enter years of service: ");
-Year = Convert.ToDouble(Console.ReadLine());
+Id = Console.ReadLine();
 
 Console.Write("Enter salary: ");
 Salary = Convert.ToDouble(Console.ReadLine());
 
+Console.Write("Enter total sales: ");
+PSale = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter commission rate [0-1]: ");
+Rate = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter number of cars sold: ");
+Num = Convert.ToDouble(Console.ReadLine());
 
 //process
-if (Year >= 3)
+var Commission = PSale * Rate;
+
+if(Num > 5)
 {
-    Bonus = Salary * 2;
+    Bonus = 9000;
 }
 
+if(Commission > Salary)
+{
+    Bonus1 = PSale * 0.1;
+}
+
+var net = Salary + Commission + Bonus + Bonus1;
+
 //output
-Console.WriteLine("Bonus: " + Bonus);
+Console.WriteLine("Commission: "+Commission);
+Console.Write("Salary: " + Salary);
+Console.WriteLine("Total Income: " + net);
