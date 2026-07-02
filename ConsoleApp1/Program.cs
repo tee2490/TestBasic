dded2039747ae3f7c65f7b55fff5d0b013be195c
tee2490 <44152787+tee2490@users.noreply.github.com>
2026-07-02 09:34:22 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿//declare
+string Code, Name;
+double Salary, Sale,Rate, Commission,Net;
+
+//input
+Console.Write("Enter code :");
+Code = Console.ReadLine();
+
+Console.Write("Enter name :");
+Name = Console.ReadLine();
+
+Console.Write("Enter salary :");
+Salary = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter sale :");
+Sale = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter rate 1-100 : ");
+Rate = Convert.ToDouble(Console.ReadLine());
+
+//process
+Commission = Sale * (Rate/100);
+Net = Salary + Commission;
+
+//output
+Console.WriteLine($"Commission = {Commission}");
+Console.WriteLine($"Net = {Net}");
+
+
+
+/// priority     + - * / ( ) จากซ้ายไปขวา ทำ (), ทำ * / ก่อน,  + -
+
+
+
