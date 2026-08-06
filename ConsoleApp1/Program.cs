a6aae64d0e9666df28d2f01f47116eb177767f46
tee2490 <44152787+tee2490@users.noreply.github.com>
2026-08-06 09:57:22 +0700

    array 1 to 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,38 +1,76 @@
-﻿Random num;
-double num1,even=0,odd=0,SumEven=0,SumOdd=0,AvgEven,AvgOdd;
+﻿
+Random num;
+int[] x;
 
+x = new int[10];
 num = new Random();
 
-var count = 0;
-for (int i = 0; i < 100; i++)
+//input array 1 dimension
+for (int i = 0; i < 10; i++)
 {
-    num1 = num.Next(20, 31);
-    Console.Write($"{num1} ");
-
-    if (num1 % 2 == 0)
-    { 
-        even++;
-        SumEven += num1;
-
-    }
-    else 
-    { 
-        odd++; 
-        SumOdd += num1;
-    }
-
-    count++;
-    if (count == 10)
-    {
-        Console.WriteLine();
-        count = 0;
-    }
-
+    x[i] = num.Next(10, 21);
 }
 
-AvgEven = SumEven / even;
-AvgOdd = SumOdd / odd;
 
-Console.WriteLine("Type\tNumber\tSum\tAverage");
-Console.WriteLine($"Even\t{even}\t{SumEven}\t{AvgEven:F2}");
-Console.WriteLine($"Odd\t{odd}\t{SumOdd}\t{AvgOdd:F2}");
+//output
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(x[i]);
+}
+
+//////////////////////////////////////////
+
+//array 2 dimension
+//เช่น นศ 10 คน มีคะแนนเก็บ 2 ครั้ง
+
+
+Console.WriteLine("2 dimension");
+int[,] score = new int[10, 2];
+
+for (int i = 0; i < 10; i++)  //นศ 10 คน
+{
+    for (int j = 0; j < 2; j++) //คะแนน 2 ครั้ง
+    {
+        score[i, j] = num.Next(10, 21);
+    }
+}
+
+
+for (int i = 0; i < 10; i++)  //นศ 10 คน
+{
+    Console.Write($"Student {i} ");
+    for (int j = 0; j < 2; j++) //คะแนน 2 ครั้ง
+    {
+        Console.Write($"{score[i, j]} ");
+    }
+
+    Console.WriteLine();
+}
+
+
+// multi dimension 3 
+//เช่น นศ 10 คน เรียน 3 วิชา แต่ละวิชาสอบ 2 ครั้ง
+
+int[,,] stud = new int[10, 3, 2];
+
+Console.WriteLine("3 dimension");
+for (int i = 0; i < 10; i++)
+    for (int j = 0; j < 3; j++)
+        for (int k = 0; k < 2; k++)
+            stud[i, j, k] = num.Next(10, 21);
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"stud {i}");
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write($"\tSubject {j} : ");
+        for (int k = 0; k < 2; k++)
+        {
+            Console.Write($"{stud[i, j, k]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
