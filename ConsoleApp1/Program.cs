9303712ad4bfb09195f464f96e352860334026b2
tee2490 <44152787+tee2490@users.noreply.github.com>
2026-08-13 10:44:19 +0700

    OOP : Services

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,21 +1,7 @@
-﻿using ConsoleApp1.Models;
+﻿using ConsoleApp1.Services;
 
-List<Product> products = new List<Product>();
-Random rnd = new Random();
+var ps = new Generate(); //oop
 
-for (int i = 0; i < 100; i++)
-{
-    var test = new Product() //สินค้า 1 รายการ
-    {
-        Id = i,
-        Name = "Product"+i,
-        Price=rnd.Next(10,21)
-    };
-
-    products.Add(test); //เพิ่มสินค้าเข้าไปใน list
-}
-
-
-products.ForEach(p => p.Display());
-
-
+//เรียกใช้ method
+ps.Create(); //สร้าง
+ps.Show();  //แสดง
---
+++ ConsoleApp1/Services/Generate.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp1.Models;
+
+namespace ConsoleApp1.Services
+{
+    public class Generate  //บริการ สร้าง แก้ ลบ ค้น CRUD
+    {
+        List<Product> products = new List<Product>();
+        Random rnd = new Random();
+
+        public void Create() //method
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                products.Add(new Product()
+                {
+                    Id = i,
+                    Name = "Prduct" + i,
+                    Price = rnd.Next(10, 21)
+                });
+            }
+
+        }
+
+
+        public void Show()
+        {
+            products.ForEach(p =>p.Display());
+        }
+
+
+    }
+}
