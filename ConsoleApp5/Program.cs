a0fdb45c33961c35cef4dba7c80e62ba6603e216
tee2490 <44152787+tee2490@users.noreply.github.com>
2026-08-26 11:50:38 +0700

    Create

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,7 +1,16 @@
-﻿using ConsoleApp5.Services;
+﻿using ConsoleApp5.Models;
+using ConsoleApp5.Services;
 
 ProductService ps = new ProductService();
-
 ps.Mock(5);
+
+Product p = new Product()
+{
+    Id = 6,
+    Name = "Mouse",
+    Price = 500
+};
+
+ps.Create(p);
 
 ps.Read();
