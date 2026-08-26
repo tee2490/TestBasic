7728be0d0bb79d51a67153fb3364a544e8342b19
tee2490 <44152787+tee2490@users.noreply.github.com>
2026-08-26 10:59:39 +0700

    Object Collections : List

---
+++ ConsoleApp5/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine($"{Id} {Name} {Price}");
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp5.Models;
+
+List<Product> products = new List<Product>();
+
+for (int i = 0; i < 10; i++)
+{
+    var temp = new Product()
+    {
+        Id = i,
+        Name = "Coffee" + i,
+        Price = 100
+    };
+
+    products.Add(temp);
+
+}
+
+products.ForEach(p => p.Display());
